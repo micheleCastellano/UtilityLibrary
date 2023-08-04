14cad6bbd3e1f052478513f49a309a079c73cf0f
Michele Castellano <129580612+micheleCastellano@users.noreply.github.com>
2023-08-04 10:55:23 +0200

    rimessa come promesso
    grazie per l'attenzione

---
+++ UtilityLibrary/MyRandomGenerator.cs
@@ -0,0 +1,18 @@
+﻿namespace UtilityLibrary
+{
+    public static class MyRandomGenerator
+    {
+        public static string GenerateAlphanumericValue(int numCaratteri)
+        {
+            string caratteri = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            Random random = new Random();
+            string valore = "";
+            for (int i = 0; i < numCaratteri; i++)
+            {
+                int indice = random.Next(caratteri.Length);
+                valore += caratteri[indice];
+            }
+            return valore;
+        }
+    }
+}
---
+++ UtilityLibrary/MyValidator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UtilityLibrary
+{
+    public static class MyValidator
+    {
+        public static bool IsEmailAddress(string email)
+        {
+            if(string.IsNullOrEmpty(email)) return false;
+            try
+            {
+                MailAddress m = new MailAddress(email);
+                return true;
+            }catch(FormatException)
+            {
+                return false;
+            }
+        }
+    }
+}
